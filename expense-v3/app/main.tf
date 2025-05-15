3ecbf3154fc901bf60d03516390b4ac1ae860126
Patil <shubham.patil5@dell.com>
2025-05-15 17:19:09 +0530

    provisioner

--- expense-v3/app/main.tf
+++ expense-v3/app/main.tf
@@ -16,3 +16,9 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.node.private_ip]
 }
 
+resource "null_resource" "provisioner" {
+  depends_on = [aws_route53_record.record]
+  provisioner "local-exec" {
+    command = "sleep 120; cd /home/ec2-user/expense-shell; ansible-playbook -i ${aws_instance.node.private_ip}, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=${var.name} -e env=dev expense.yml"
+  }
+}
