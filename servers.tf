31439875dd0d39fbaf471a52ee524fc83b6a8be2
Patil <shubham.patil5@dell.com>
2025-05-15 11:26:19 +0530

    Create servers

--- servers.tf
+++ servers.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0c2fba399b8205ad3"]
 
   tags = {
     Name = "frontend"
@@ -10,6 +11,7 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "mysql" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0c2fba399b8205ad3"]
 
   tags = {
     Name = "mysql"
@@ -19,6 +21,7 @@ resource "aws_instance" "mysql" {
 resource "aws_instance" "backend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0c2fba399b8205ad3"]
 
   tags = {
     Name = "backend"
