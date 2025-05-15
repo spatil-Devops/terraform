3b74d25a7ab9ec28c6a366c4c53149f2f3133961
Patil <shubham.patil5@dell.com>
2025-05-15 16:01:49 +0530

    list required for sg

--- expense-v3/main.tf
+++ expense-v3/main.tf
@@ -5,7 +5,7 @@ module "app" {
   ami = data.aws_ami.ami.image_id
   name = each.key
   instance_type = each.value["instance_type"]
-  vpc_security_group_ids = data.aws_security_group.allow-all.id
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
   zone_id = data.aws_route53_zone.zone.zone_id
 }
 
